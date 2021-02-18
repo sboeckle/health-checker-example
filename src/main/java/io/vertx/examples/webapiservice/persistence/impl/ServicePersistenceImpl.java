35a4decc7f203872be4a5d9b26548691bce543e0
steven boeckle <steven.boeckle@coliquio.de>
2021-02-18 10:21:31 +0100

    make write to file and read from it again work

--- src/main/java/io/vertx/examples/webapiservice/persistence/impl/ServicePersistenceImpl.java
+++ src/main/java/io/vertx/examples/webapiservice/persistence/impl/ServicePersistenceImpl.java
@@ -2,11 +2,17 @@ package io.vertx.examples.webapiservice.persistence.impl;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.examples.webapiservice.models.Service;
 import io.vertx.examples.webapiservice.persistence.ServicePersistence;
+
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.json.JSONObject;
 
@@ -14,39 +20,45 @@ public class ServicePersistenceImpl implements ServicePersistence {
 
     private Vertx vertx;
     private final String fileName = "services.serialized";
-    private SortedMap <String, Service> services;
+    private Map <String, Service> services;
 
     public ServicePersistenceImpl(Vertx vertx) {
         this.vertx = vertx;
-        services = new TreeMap<String, Service>();
-        initPersistance();
+        services = new HashMap<String, Service>();
+        readFromFile();
     }
 
-    private void initPersistance() {
-        vertx.fileSystem().readFile(fileName, ar -> {
-          if (ar.failed()) {
-            System.out.println(ar.cause().getMessage());
-          } else {
-            System.out.println("WORKED SO FAR");
-          }
-        });
-    }
-
-    private void saveToFile() {
-      JSONObject json = new JSONObject(services);
-      System.out.println(json.toString());
-      vertx.fileSystem().writeFile(
-          fileName,
-          Buffer.buffer(json.toString()),
-          result -> {
-            if (result.succeeded()) {
-              System.out.println("wrote it" + result.toString());
-            } else {
-              System.err.println("did" +
-                  " not write it");
-            }
+    private void readFromFile() {
+        try {
+          String servicesAsString = null;
+          servicesAsString = Files.readString(Path.of(fileName));
+          JSONObject jsonO = new JSONObject(servicesAsString);
+          jsonO.keySet().forEach(k -> {
+            JSONObject o = (JSONObject) jsonO.get(k);
+            Service s = new Service(new JsonObject(o.toString()));
+            services.put(k, s);
           });
+        } catch (IOException e) {
+          System.err.println("EROROROR: " + e.getMessage());
+        }
     }
+
+      private void saveToFile() {
+        // use JSONObject for simple parsing of Map -> String
+        JSONObject jsonO = new JSONObject(services);
+        JsonObject json = new JsonObject(jsonO.toString());
+        vertx.fileSystem().writeFile(
+            fileName,
+            Buffer.buffer(json.toString()),
+            result -> {
+              if (result.succeeded()) {
+                System.out.println("wrote it" + result.toString());
+              } else {
+                System.err.println("did" +
+                    " not write it");
+              }
+            });
+      }
 
     @Override
     public List<Service> getFilteredServices(Predicate<Service> p) {
@@ -55,6 +67,7 @@ public class ServicePersistenceImpl implements ServicePersistence {
 
     @Override
     public Optional<List<Service>> getServices() {
+      readFromFile();
       return Optional.of(new ArrayList<>(services.values()));
     }
 
